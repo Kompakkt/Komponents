307d79d3a56a2660ebf297f60ad30ff38019b833
Kai Niebes <kai.niebes@outlook.com>
2025-04-08 10:33:41 +0200

    Add autocomplete wrapper

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@
  * Public API Surface of komponents
  */
 
-export * from './button-row/button-row.component';
-export * from './button/button.component';
-export * from './details/details.component';
-export * from './input/input.component';
-export * from './labelled-checkbox/labelled-checkbox.component';
-export * from './slide-toggle/slide-toggle.component';
-export * from './slider/slider.component';
-export * from './textarea/textarea.component';
-export * from './tooltip.directive';
-export * from './wizard-step/wizard-step.component';
-export * from './wizard/wizard.component';
-export * from './menu/menu.component';
-export * from './menu-option/menu-option.component';
+export * from "./lib/button-row/button-row.component";
+export * from "./lib/button/button.component";
+export * from "./lib/details/details.component";
+export * from "./lib/input/input.component";
+export * from "./lib/labelled-checkbox/labelled-checkbox.component";
+export * from "./lib/slide-toggle/slide-toggle.component";
+export * from "./lib/slider/slider.component";
+export * from "./lib/textarea/textarea.component";
+export * from "./lib/tooltip.directive";
+export * from "./lib/wizard-step/wizard-step.component";
+export * from "./lib/wizard/wizard.component";
+export * from "./lib/menu/menu.component";
+export * from "./lib/menu-option/menu-option.component";
+export * from "./lib/autocomplete/autocomplete.component";
---
+++ src/lib/autocomplete/autocomplete.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, input } from "@angular/core";
+import { InputComponent } from "../input/input.component";
+
+@Component({
+  selector: "lib-autocomplete",
+  standalone: true,
+  imports: [],
+  templateUrl: "./autocomplete.component.html",
+  styleUrl: "./autocomplete.component.scss",
+})
+export class AutocompleteComponent {
+  inputElement = input.required<InputComponent>();
+  opened = computed(() => {
+    const input = this.inputElement();
+    return input.focused();
+  });
+}
