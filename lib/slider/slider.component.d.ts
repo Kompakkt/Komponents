ba989ea4edf3cdb1c36f118482da16c021613989
HeyItsBATMAN <kai.niebes@outlook.com>
2025-11-13 17:45:08 +0000

    Build output: Add direction option to slider

--- lib/slider/slider.component.d.ts
+++ lib/slider/slider.component.d.ts
@@ -7,10 +7,12 @@ export declare class SliderComponent implements AfterViewInit, OnInit, OnDestroy
     min: import("@angular/core").InputSignal<number>;
     max: import("@angular/core").InputSignal<number>;
     startingValue: import("@angular/core").InputSignal<number>;
+    showLabel: import("@angular/core").InputSignal<boolean>;
     step: import("@angular/core").InputSignal<number>;
     value: import("@angular/core").WritableSignal<number>;
     value$: import("rxjs").Observable<number>;
     valueChanged: import("@angular/core").OutputEmitterRef<number>;
+    direction: import("@angular/core").InputSignal<"left-to-right" | "bottom-to-top">;
     startingValueChangedEffect: import("@angular/core").EffectRef;
     showInfo: import("@angular/core").InputSignal<boolean>;
     showTooltip: import("@angular/core").InputSignal<boolean>;
@@ -22,7 +24,6 @@ export declare class SliderComponent implements AfterViewInit, OnInit, OnDestroy
     ngOnInit(): void;
     ngOnDestroy(): void;
     ngAfterViewInit(): void;
-    get showInfoClass(): boolean;
     static ɵfac: i0.ɵɵFactoryDeclaration<SliderComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<SliderComponent, "k-slider", never, { "label": { "alias": "label"; "required": true; "isSignal": true; }; "min": { "alias": "min"; "required": false; "isSignal": true; }; "max": { "alias": "max"; "required": false; "isSignal": true; }; "startingValue": { "alias": "startingValue"; "required": false; "isSignal": true; }; "step": { "alias": "step"; "required": false; "isSignal": true; }; "showInfo": { "alias": "showInfo"; "required": false; "isSignal": true; }; "showTooltip": { "alias": "showTooltip"; "required": false; "isSignal": true; }; }, { "valueChanged": "valueChanged"; }, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<SliderComponent, "k-slider", never, { "label": { "alias": "label"; "required": true; "isSignal": true; }; "min": { "alias": "min"; "required": false; "isSignal": true; }; "max": { "alias": "max"; "required": false; "isSignal": true; }; "startingValue": { "alias": "startingValue"; "required": false; "isSignal": true; }; "showLabel": { "alias": "showLabel"; "required": false; "isSignal": true; }; "step": { "alias": "step"; "required": false; "isSignal": true; }; "direction": { "alias": "direction"; "required": false; "isSignal": true; }; "showInfo": { "alias": "showInfo"; "required": false; "isSignal": true; }; "showTooltip": { "alias": "showTooltip"; "required": false; "isSignal": true; }; }, { "valueChanged": "valueChanged"; }, never, never, true, never>;
 }
