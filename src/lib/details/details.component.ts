3d4ccd26772a9da60f2cf18420d5dfbd3f216013
Kai Niebes <kai.niebes@outlook.com>
2026-07-07 15:17:54 +0200

    Fix: Resizing of WizardStepComponent and DetailsComponent were based on janky intervals, instead of using the viewChild

--- src/lib/details/details.component.ts
+++ src/lib/details/details.component.ts
@@ -9,7 +9,6 @@ import {
   signal,
   viewChild,
 } from '@angular/core';
-import { interval } from 'rxjs';
 
 @Component({
   selector: 'k-details',
@@ -25,9 +24,7 @@ export class DetailsComponent implements AfterViewInit {
   expanded = signal(true);
   gap = input(8);
 
-  contentDiv = viewChild<ElementRef<HTMLDivElement>>('detailsContent');
   titleEl = viewChild<ElementRef<HTMLSpanElement>>('titleEl');
-  contentHeight = signal('auto');
 
   constructor() {
     const destroyRef = inject(DestroyRef);
@@ -61,27 +58,10 @@ export class DetailsComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    // TODO: this should only be triggered if the content somehow changes height.
-    // However, I could not get this to work with MutationObserver or ngChanges
-    // - Kai
-    const resize = () =>
-      requestAnimationFrame(() => {
-        if (!this.expanded()) return;
-        const clientHeight = this.contentDiv()?.nativeElement.clientHeight;
-        if (!clientHeight) return;
-        const oldHeight = +this.contentHeight().replace('px', '').replace('auto', '0');
-        if (clientHeight <= oldHeight) return;
-        this.contentHeight.set(clientHeight + 'px');
-      });
-
-    resize();
-
     if (this.alwaysExpanded()) {
       this.expanded.set(true);
     } else if (this.startCollapsed()) {
       this.expanded.set(false);
     }
-
-    interval(100).subscribe(() => resize());
   }
 }
--- src/lib/wizard-step/wizard-step.component.ts
+++ src/lib/wizard-step/wizard-step.component.ts
@@ -1,5 +1,4 @@
 import {
-  AfterViewInit,
   Component,
   ElementRef,
   HostBinding,
@@ -8,7 +7,6 @@ import {
   signal,
   viewChild,
 } from '@angular/core';
-import { interval } from 'rxjs';
 
 @Component({
   selector: 'k-wizard-step',
@@ -17,7 +15,7 @@ import { interval } from 'rxjs';
   templateUrl: './wizard-step.component.html',
   styleUrl: './wizard-step.component.scss',
 })
-export class WizardStepComponent implements AfterViewInit {
+export class WizardStepComponent {
   label = input.required<string>();
   active = signal(false);
   completed = signal(false);
@@ -26,7 +24,6 @@ export class WizardStepComponent implements AfterViewInit {
   direction = signal<'horizontal' | 'vertical'>('horizontal');
 
   contentDiv = viewChild<ElementRef<HTMLDivElement>>('stepContent');
-  contentHeight = signal('auto');
 
   headerClicked = output<void>();
 
@@ -43,24 +40,5 @@ export class WizardStepComponent implements AfterViewInit {
   @HostBinding('class.horizontal')
   get isHorizontal() {
     return this.direction() === 'horizontal';
-  }
-
-  ngAfterViewInit(): void {
-    // TODO: this should only be triggered if the content somehow changes height.
-    // However, I could not get this to work with MutationObserver or ngChanges
-    // - Kai
-    const resize = () =>
-      requestAnimationFrame(() => {
-        if (!this.active()) return;
-        const clientHeight = this.contentDiv()?.nativeElement.clientHeight;
-        if (!clientHeight) return;
-        const oldHeight = +this.contentHeight().replace('px', '').replace('auto', '0');
-        if (clientHeight <= oldHeight) return;
-        this.contentHeight.set(clientHeight + 'px');
-      });
-
-    resize();
-
-    interval(100).subscribe(() => resize());
   }
 }
